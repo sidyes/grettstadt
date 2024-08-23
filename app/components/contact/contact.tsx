a3f2c80f648dacdce71612bc9478aca337eaeef0
Stefan Herpich <s10h@posteo.de>
2024-08-23 19:26:15 +0200

    fix contact

--- app/components/contact/contact.tsx
+++ app/components/contact/contact.tsx
@@ -1,23 +1,44 @@
+import { useState } from "react";
+
 export default function Contact() {
+  const [status, setStatus] = useState<string>();
+  const [error, setError] = useState<string>();
+
+  const handleFormSubmit = async (event: any) => {
+    event.preventDefault();
+    try {
+      setStatus("pending");
+      setError(undefined);
+      const myForm = event.target;
+      const formData = new FormData(myForm);
+      const res = await fetch("/__forms.html", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData as any).toString(),
+      });
+      if (res.status === 200) {
+        setStatus("ok");
+      } else {
+        setStatus("error");
+        setError(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      setStatus("error");
+      setError(`${e}`);
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-6 rounded-md">
       <h2 className="text-2xl font-bold mb-6 text-gray-800">Kontakt</h2>
       <form
+        onSubmit={handleFormSubmit}
         name="contact"
         method="POST"
-        data-netlify="true"
-        data-netlify-recaptcha="true"
         className="space-y-4"
-        action="/danke"
       >
         {/* Netlify Form Hidden Input */}
         <input type="hidden" name="form-name" value="contact" />
-        <p className="hidden">
-          <label>
-            Don’t fill this out if you’re human: <input name="bot-field" />
-          </label>
-        </p>
-
         <div>
           <label
             htmlFor="name"
@@ -75,6 +96,56 @@ export default function Contact() {
           </button>
         </div>
       </form>
+      {status === "ok" && (
+        <div className="alert alert-success">
+          <SuccessIcon />
+          <p className="text-gray-700 text-lg">
+            Ihre Nachricht wurde erfolgreich gesendet. <br />
+            Wir werden uns so schnell wie möglich bei Ihnen melden.
+          </p>
+        </div>
+      )}
+      {status === "error" && (
+        <div className="alert alert-error">
+          <ErrorIcon />
+          {error}
+        </div>
+      )}
     </div>
+  );
+}
+
+function SuccessIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="stroke-current shrink-0 h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
+function ErrorIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="stroke-current shrink-0 h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
   );
 }
